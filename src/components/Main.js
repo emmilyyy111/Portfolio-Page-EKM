9013c3e8a155e8e65b7629756b18ac7dd6bb6a9a
Emily Morgenbesser <emilymorgenbesser@Emilys-MacBook-Pro.local>
2021-08-15 16:58:00 -0400

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,8 @@
+export default function Main () {
+    return (
+        <div className="main">  
+        <h1>Software Engineer</h1>
+        <h3> portfolio pics here in b&w </h3>
+        </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar () {
+    return(
+        <nav className="navbar">
+            <h1 className="nav-title">Emily Morgenbessers Portfolio</h1>
+            <div className="nav-links">
+                <a href='/'>Home</a>
+                <a href='/about'>About</a>
+                <a href='/portfolio'>Portfolio</a>
+                <a href='/resume'>Resume</a>
+                <a href='/contact'>Contact</a>
+            </div>
+        </nav>
+    )
+}
